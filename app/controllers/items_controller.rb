465fe1705653a4070f5c3deb62eb803f75128681
seriten <pg_mymelody@icloud.com>
2020-09-28 16:54:09 +0900

    コードレビュー修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,8 +26,11 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-      @item.destroy
+      if @item.destroy
       redirect_to root_path
+      else
+        render :show
+      end
     end
 
     def update
