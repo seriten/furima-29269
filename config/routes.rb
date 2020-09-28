9817f707e0aee7499e6c52695a1043ba36dab54b
seriten <pg_mymelody@icloud.com>
2020-09-28 11:53:27 +0900

    編集ページへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root 'items#index'
 
   
-  resources :items, only: [:index, :new, :create, :edit, :show]
+  resources :items, only: [:index, :new, :create, :edit, :show, :edit, :update]
 
 end
-
